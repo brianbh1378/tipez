3f2b3b0bc9d57c31979cfa2d62951cdb0ea92eda
Brian Sai Kit <brianbh1378@gmail.com>
2016-12-27 12:08:47 -0800

    finished tip calculator

--- tipez/ViewController.swift
+++ tipez/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var numberOfPeople: UITextField!
+    @IBOutlet weak var priceForEach: UILabel!
+    
+    var total : Double = 0
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,51 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        // Disappearing keyboard after clicking on main screen
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(_ sender: Any) {
+        // Array to hold different tip percentage
+        let tipPercentages = [0.18, 0.2, 0.25]
+    
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        priceForEach.text = String(format: "$%.2f" + " /Person", total)
+    }
+    
+    // This function calls when the segmented option ischanged
+    @IBAction func differentTipPercent(_ sender: Any) {
+        calculateTip(Any.self)
+    }
+    
+    // This function is called when # of people
+    // is enter
+    @IBAction func calculateForEach(_ sender: Any) {
+        let numOfPeople = Double(numberOfPeople.text!) ?? 0
+        if numOfPeople == 0{
+            priceForEach.text = String(format: "$%.2f" + " /Person", total)
+        }
+        else{
+            let amount = total / numOfPeople
+            priceForEach.text = String(format: "$%.2f" + " /Person", amount)
+        }
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
 
